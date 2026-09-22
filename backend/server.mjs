35997f45dd006f873d73491c9d457d6291b954d2
Sanjayashok2006 <sanjayashok1122@gmail.com>
2026-09-22 22:23:01 +0530

    minor tweaks

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,19 +1,17 @@
 import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
 import expenseRouter from "./routes/expenseRoutes.mjs";
 
 const app = express();
 
-dotenv.config();
 
 mongoose.connect("mongodb://localhost:27017/expense_tracker")
     .then(() => console.log("DB Connected"))
     .catch((err) => console.log(err));
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Expense Tracker Backend is running");
